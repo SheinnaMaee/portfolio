5728f51f165597844531f64da147dce2495894fa
Md Hussain <>
2021-04-26 15:51:50 +0000

    build

---
+++ build/precache-manifest.7408fb2507a1c10231b44e041cde3b2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8700f610b6fb986c714f47e27d9197",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c76ea7e3ea69d259995",
+    "url": "/static/css/main.d79fefd5.chunk.css"
+  },
+  {
+    "revision": "e3356fc90d2e7a51a81a",
+    "url": "/static/js/2.099a8bbc.chunk.js"
+  },
+  {
+    "revision": "4c76ea7e3ea69d259995",
+    "url": "/static/js/main.89ee905e.chunk.js"
+  },
+  {
+    "revision": "f67c199efb9f2c29f389",
+    "url": "/static/js/runtime-main.f294effb.js"
+  },
+  {
+    "revision": "3f1c62337eaa920a98080dbdaad6ce91",
+    "url": "/static/media/AnimatedShape.3f1c6233.svg"
+  }
+]);
