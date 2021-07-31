28f61aec9c7d6b587b35e7cd209bc921241a3ecc
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-08-01 00:26:48 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,20 @@
-import React,{useState} from "react"
-import Reveal from 'react-reveal/Reveal';
-
+import React from "react"
+import Fade from 'react-reveal/Fade';
 const About = () => {
-  const [showText,setShowText] = useState(false)
-  window.addEventListener('scroll',()=>{
-    if(window.scrollY > 600){
-      setShowText(true)
-    } 
-  })
+
   return (
     <div>
-    <Reveal effect="fadeInUp">
+
       <section className="about" id="about">
-			<p id="about_text" className={ showText ? "text-focus-in" : ""}>
+      <Fade left cascade className="about-container">
+			<p id="about_text">
 				I'm a full stack developer based in Delhi,India with
 				determination for delivering end-to-end Web solutions. I'm
 				passionate about improving the lives of others through my
 				products and am constantly looking to learn new things everyday.
 			</p>
+      </Fade>
 		</section>
-    </Reveal>
     </div>
   )
 }
