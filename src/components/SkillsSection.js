e3dca221abc61e3f88649dde8427dabcef5046bf
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-08-01 00:27:25 +0530

    Update SkillsSection.js

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -2,9 +2,9 @@ import React from "react"
 import Responsive from "./Slider"
 
 const SkillsSection = () => {
-  return(<div>
-  <Responsive/>
-  </div>)
+  return(
+  <Responsive className="slider"/>
+)
 }
 
 export default SkillsSection
