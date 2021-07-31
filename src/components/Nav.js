f75894c68674058f350f4969c602f4ef6e7d609a
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-08-01 00:26:31 +0530

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,40 +1,66 @@
-import React, { useState } from "react"
-
+import React, { useState } from "react";
+import Fade from "react-reveal/Fade";
 
 const Nav = () => {
   const [isActive, setActive] = useState(false);
-  const [scroll, setScroll] = useState(false)
+  const [scroll, setScroll] = useState(false);
   const ToggleClass = () => {
     setActive(!isActive);
-    console.log(isActive)
+    console.log(isActive);
   };
   const is_scrolling = () => {
     if (window.scrollY > 0) {
-      setScroll(true)
+      setScroll(true);
     } else {
-      setScroll(false)
+      setScroll(false);
     }
-  }
-  window.addEventListener('scroll', is_scrolling)
+  };
+  window.addEventListener("scroll", is_scrolling);
   return (
     <div>
-      <header className="main-head">
-        <nav id="main-header" className={`${scroll ? "scrolling-active" : ''} ${isActive ? "nav-background" : ""}`}>
-          <h2 className="bounce-in-top"><a href="#home">vgoat.</a></h2>
-
-          <div className={`collapse ${isActive ? "burger-active" : ""}`} >
-            <h4 className="bounce-in-top"><a href="#about">About</a></h4>
-            <h4 className="bounce-in-top">
-              <a href="#projects">Projects</a>
+      <header className={`main-head ${scroll ? "scrolling-active" : ""} ${
+            isActive ? "nav-background" : ""
+          }`}>
+        <nav
+          id="main-header"
+        >
+        
+            <h2>
+            <a href="#home">vgoat.</a>
+            </h2>
+          
+          <div className={`right collapse ${isActive ? "burger-active" : ""}`}>
+            <h4>
+              <Fade left cascade>
+                <a href="#about">About</a>
+              </Fade>
             </h4>
-            <h4 className="bounce-in-top">
-              <a href="#contact">Contact</a>
+            <h4>
+              <Fade left cascade>
+                <a href="#projects">Project</a>
+              </Fade>
             </h4>
-            <h4 className="bounce-in-top">
-              <a href={require('./docs/mycv.pdf')} target="_blank" rel="noopener noreferrer"> Resume</a>
+            <h4>
+              <Fade left cascade>
+                <a href="#contact">Contact</a>
+              </Fade>
+            </h4>
+            <h4>
+              <Fade left cascade>
+                <a
+                  href={require("./docs/mycv.pdf")}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Resume
+                </a>
+              </Fade>
             </h4>
           </div>
-          <div className={`burger ${isActive ? "toggle" : ""}`} onClick={ToggleClass}>
+          <div
+            className={`burger ${isActive ? "toggle" : ""}`}
+            onClick={ToggleClass}
+          >
             <div className="line1"></div>
             <div className="line2"></div>
             <div className="line3"></div>
@@ -42,7 +68,7 @@ const Nav = () => {
         </nav>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
